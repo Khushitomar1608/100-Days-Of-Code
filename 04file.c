c233e4bade33fb4cc95ada059bb01e90e16dfebd
Khushitomar1608 <khushitmr1608@gmail.com>
2025-11-26 04:44:10 +0530

    User inputs, operations and output

---
+++ 04file.c
@@ -0,0 +1,16 @@
+//Write a program to calculate the area and circumference of a circle given its radius.
+
+#include<stdio.h>
+#define PI 3.14
+
+int main(){
+    float r;
+
+    printf("Enter the radius of the circle\n");
+    scanf("%f",&r);
+
+    printf("Area=%.2f, ", PI*r*r);
+    printf("Circumference=%.2f\n", 2*PI*r);
+
+    return 0;
+}
