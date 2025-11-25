ee37d1edd4b3a464742b147b236c813e6b7e3324
Khushitomar1608 <khushitmr1608@gmail.com>
2025-11-26 04:48:01 +0530

    User inputs, operations and output

---
+++ 09file.c
@@ -0,0 +1,20 @@
+//Write a program to calculate simple and compound interest for given principal, rate, and time.
+
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double p,r,t,A;
+
+    printf("Enter the principal amount, rate and time:\n");
+    scanf("%lf %lf %lf" ,&p, &r, &t);
+
+    printf("Simple Interest=%.2lf, ",(p*r*t)/100);
+
+    A= p*pow((1+r/100),t);
+
+    printf("Compound Interest=%.2lf", A-p);
+
+    return 0;
+}
