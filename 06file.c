a2e12220e0342f09008279163a82d61a4077dc0a
Khushitomar1608 <khushitmr1608@gmail.com>
2025-11-26 04:45:26 +0530

    User inputs, operations and output

---
+++ 06file.c
@@ -0,0 +1,18 @@
+//Write a program to swap two numbers using a third variable.
+
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+
+    printf("Enter two numbers\n");
+    scanf("%d %d",&a,&b);
+
+    c=a;
+    a=b;
+    b=c;
+
+    printf("After swap:%d %d",a,b);
+
+    return 0;
+}
