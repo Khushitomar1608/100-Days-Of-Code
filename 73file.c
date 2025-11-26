46951fdf305636f518fa601e2d7346818736b0a2
Khushitomar1608 <khushitmr1608@gmail.com>
2025-11-26 08:50:06 +0530

    2D Arrays

---
+++ 73file.c
@@ -0,0 +1,25 @@
+//Find the sum of each row of a matrix and store it in an array.
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int a[r][c], rowSum[r];
+
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    for(int i = 0; i < r; i++) {
+        int sum = 0;
+        for(int j = 0; j < c; j++)
+            sum += a[i][j];
+        rowSum[i] = sum;
+    }
+
+    for(int i = 0; i < r; i++)
+        printf("%d ", rowSum[i]);
+
+    return 0;
+}
