d8481b325d693dfd48f7e86b2ede1d05be8abf1d
Khushitomar1608 <khushitmr1608@gmail.com>
2025-11-27 23:17:31 +0530

    Create 100file.c

---
+++ 100file.c
@@ -0,0 +1,27 @@
+//Print all sub-strings of a string.
+  
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int len, i, j, k;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+    for (i = 0; i < len; i++) {
+        for (j = i; j < len; j++) {
+            for (k = i; k <= j; k++) {
+                printf("%c", str[k]);
+            }
+            if (!(i == len - 1 && j == len - 1)) // Avoid trailing comma
+                printf(",");
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
